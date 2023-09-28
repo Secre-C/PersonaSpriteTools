7db3eed3a3a56c06ffc803c37509ee83442b61a0
SecreC <gabirocks505@gmail.com>
2023-09-27 23:35:07 -0700

    make imagemagick processes multithreaded

--- SpdDisassembler.py
+++ SpdDisassembler.py
@@ -1,8 +1,12 @@
 import os
 import sys
 from Spd.spd import spd
+import multiprocessing 
 
 def disassemble_spd_file(input_spd, output_folder):
+    # Set maximum processes. Change to 4 if you experience crashing
+    MAX_PROCESSES = 8
+    
     # Parse Spd
     spd_data = spd(input_spd)
 
@@ -13,24 +17,41 @@ def disassemble_spd_file(input_spd, output_folder):
     # write textures to output path
     for key, value in spd_data.texture_data_dict.items():
         write_dds_to_file(value, f'{output_folder}\\tex_{key}.dds')
+    
+    # le epic chat gpt copy paste
+    if __name__ == "__main__":
+        # Write spdsprs to output path, and cut sprites from dds files
+        with multiprocessing.Pool(processes=MAX_PROCESSES) as pool:
+            # Use the map method to process each sprite in parallel
+            results = []
+            for key, value in spd_data.sprite_dict.items():
+                result = pool.apply_async(process_sprite, (key, value, output_folder))
+                results.append(result)
 
-    # Write spdsprs to output path, and cut sprites from dds files
-    for key, value in spd_data.sprite_dict.items():
-        print(f"Exporting sprite id {value.sprite_id}")
+            # Wait for all processes to complete
+            pool.close()
+            pool.join()
 
-        # prepare information to cut sprite from texture 
-        texture_path = f'{output_folder}\\tex_{value.sprite_texture_id}.dds'
-        texture_output = f'{output_folder}\\spr_{value.sprite_id}.dds'
+            # Get any potential exceptions raised by the processes
+            for result in results:
+                result.get()
 
-        # cut sprite from texture
-        cut_image(texture_path, value.sprite_x_position, value.sprite_y_position, value.sprite_x_length, value.sprite_y_length, texture_output)
+def process_sprite(key, value, output_folder):
+    print(f"Exporting sprite id {value.sprite_id}")
 
-        # zero out sprite x and y 
-        value.sprite_x_position = 0
-        value.sprite_y_position = 0
+    # Prepare information to cut sprite from texture
+    texture_path = f'{output_folder}\\tex_{value.sprite_texture_id}.dds'
+    texture_output = f'{output_folder}\\spr_{value.sprite_id}.dds'
 
-        # write spdspr to file
-        write_spr_to_file(value, f'{output_folder}\\spr_{key}.spdspr') 
+    # Cut sprite from texture
+    cut_image(texture_path, value.sprite_x_position, value.sprite_y_position, value.sprite_x_length, value.sprite_y_length, texture_output)
+
+    # Zero out sprite x and y
+    value.sprite_x_position = 0
+    value.sprite_y_position = 0
+
+    # Write spdspr to file
+    write_spr_to_file(value, f'{output_folder}\\spr_{key}.spdspr')
 
 def write_dds_to_file(dds_bytes, output_file):
     file = open(output_file, 'wb')
@@ -74,4 +95,3 @@ else:
     output_folder = f'{input_spd}_out'
 
 disassemble_spd_file(input_spd, output_folder)
-    
